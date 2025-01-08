4c8d7272be55de6abf377a04739266417ba8f2b0
Muhammad Rashid <rashidnaich88@gmail.com>
2025-01-08 18:10:36 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var todolist = ["Morning prayers","Prepare breakfast","Complete frontend practice"]
+
+todolist.push("Review HTML, CSS," , "Watch React.js tutorial")
+todolist.pop()
+todolist.shift()
+todolist.unshift("Research Upwork gigs", "Take a 15-minute walk","Organize your laptop files" )
+
+
+
+console.log(todolist)
